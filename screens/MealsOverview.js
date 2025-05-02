ecacefa1dce6b25e37778de91ff3f20036cdaab4
Ayeshmantha Madara Wijayagunethilake <ayesh99747@gmail.com>
2025-05-02 16:39:57 +0100

    Added functionality to display individual meals.

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,21 +1,32 @@
-import {Text, View} from "react-native";
+import {FlatList, StyleSheet, View} from "react-native";
+import {MEALS} from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 
 export default function MealsOverviewScreen({route}) {
     const {categoryId} = route.params;
+
+    const displayedMeals = MEALS.filter((meal) => {
+        return meal.categoryIds.indexOf(categoryId) >= 0;
+    });
+
+    function renderMealItem(itemData) {
+        return (
+            <View>
+                <MealItem item={itemData.item}/>
+            </View>
+        );
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Meals Overview Screen</Text>
-            <Text>Category ID: {categoryId}</Text>
-        </View>
+        <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}
+        />
     );
 }
 
-const styles = {
+const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
+        padding: 16,
     },
-};
+});
